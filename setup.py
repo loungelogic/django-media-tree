cf42d90b87ec189e6515faa92896fb6a2287751d
Adam Avramov <fallenblood@gmail.com>
2014-01-07 16:57:58 +0000

    Update layout of setup.py, too.
    Yay PEP8

--- setup.py
+++ setup.py
@@ -7,29 +7,27 @@ def read(fname):
 
 README = read('README.rst')
 
-setup(
-    name = "django-media-tree",
-    version = "0.7.2",
-    url = 'http://github.com/philomat/django-media-tree',
-    license = 'BSD',
-    description = "Django Media Tree is a Django app for managing your website's media files in a folder tree, and using them in your own applications.",
-    long_description = README,
+setup(name="django-media-tree",
+      version="0.7.2",
+      url='http://github.com/philomat/django-media-tree',
+      license='BSD',
+      description=("Django Media Tree is a Django app for managing your "
+                   "website's media files in a folder tree, and using them "
+                   "in your own applications."),
+      long_description=README,
 
-    author = u'Samuel Luescher',
-    author_email = 'sam at luescher dot org',
+      author=u'Samuel Luescher',
+      author_email='sam at luescher dot org',
 
-    packages = find_packages(),
-    include_package_data=True,
+      packages=find_packages(),
+      include_package_data=True,
 
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-    ]
-)
+      classifiers=['Development Status :: 4 - Beta',
+                     'Framework :: Django',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: BSD License',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python',
+                     'Topic :: Internet :: WWW/HTTP',])
 
 print find_packages()
