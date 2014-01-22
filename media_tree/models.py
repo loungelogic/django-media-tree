706ac5344788372e379308a19b23ff037311b4e0
Adam Avramov <fallenblood@gmail.com>
2014-01-22 13:46:01 +0000

    Fix default manager for FileNode, and move documentation in place.

--- media_tree/models.py
+++ media_tree/models.py
@@ -811,36 +811,6 @@ class AdminMixin(models.Model):
 
 
 # class SimpleFileNode(FolderMixin, BaseNode):
-#     """ Each ``FileNode`` instance represents a node in the media object tree,
-#         that is to say a “file” or “folder”. Accordingly, their ``node_type``
-#         attribute can either be ``media_types.FOLDER``, meaning that they may
-#         have child nodes, or ``FileNode.FILE``, meaning that they are
-#         associated to media files in storage and are storing metadata about
-#         those files.
-
-#         .. Note::
-#            Since ``FileNode`` is a child class of ``MPTTModel``, it inherits
-#            many methods that facilitate queries and data manipulation when
-#            working with trees.
-
-#         You can access the actual media associated to a ``FileNode`` model
-#         instance  using the following fields:
-
-#         .. role:: descname(literal)
-#            :class: descname 
-
-#         :descname:`file`
-#             The actual media file
-
-#         :descname:`preview_file`
-#             An optional image file that will be used for previews. This is
-#             useful  for visual media that PIL cannot read, such as video files.
-
-#         These fields are of the class ``FileField``. Please see
-#         :ref:`configuration` for information on how to configure storage and
-#         media backend classes. By default, media files are stored in a
-#         subfolder ``uploads`` under your media root. """
-
 #     class Meta:
 #         managed = app_settings.MEDIA_TREE_MODEL == 'media_tree.SimpleFileNode'
 #         verbose_name = _('file node')
@@ -849,6 +819,36 @@ class AdminMixin(models.Model):
 
 class FileNode(FolderMixin, MetadataMixin, FileInfoMixin, ImageMixin, 
                PositionMixin, LinkMixin, AdminMixin, BaseNode):
+    """ Each ``FileNode`` instance represents a node in the media object tree,
+        that is to say a “file” or “folder”. Accordingly, their ``node_type``
+        attribute can either be ``media_types.FOLDER``, meaning that they may
+        have child nodes, or ``FileNode.FILE``, meaning that they are
+        associated to media files in storage and are storing metadata about
+        those files.
+
+        .. Note::
+           Since ``FileNode`` is a child class of ``MPTTModel``, it inherits
+           many methods that facilitate queries and data manipulation when
+           working with trees.
+
+        You can access the actual media associated to a ``FileNode`` model
+        instance  using the following fields:
+
+        .. role:: descname(literal)
+           :class: descname 
+
+        :descname:`file`
+            The actual media file
+
+        :descname:`preview_file`
+            An optional image file that will be used for previews. This is
+            useful  for visual media that PIL cannot read, such as video files.
+
+        These fields are of the class ``FileField``. Please see
+        :ref:`configuration` for information on how to configure storage and
+        media backend classes. By default, media files are stored in a
+        subfolder ``uploads`` under your media root. """
+
     class Meta:
         managed = app_settings.MEDIA_TREE_MODEL == 'media_tree.FileNode'
 
@@ -856,7 +856,7 @@ class FileNode(FolderMixin, MetadataMixin, FileInfoMixin, ImageMixin,
         super(FileNode, self).__init__(*args, **kwargs)
 
         # HACK: Override default manager
-        self.__class__._default_manager = self.__class__.objects
+FileNode._default_manager = FileNode.objects
         
 mptt.register(FileNode)
 
